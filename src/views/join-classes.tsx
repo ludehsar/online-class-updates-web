07dffd3f800ea093a1dae583e2d1c0bec397b6b4
Rashedul Alam <rashedulalam112112@gmail.com>
2020-10-26 13:44:11 +0600

    Improved join class form design

--- src/views/join-classes.tsx
+++ src/views/join-classes.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { List, Datagrid, TextField, UrlField, EditButton, Create, Edit, TextInput, SimpleForm, required } from 'react-admin';
-import { useMediaQuery } from '@material-ui/core';
+import { Grid, Typography, useMediaQuery } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import { MobileGridAsAdmin, MobileGridAsUser } from '../components/join-classes/MobileGrid';
+import { Styles } from '@material-ui/core/styles/withStyles';
 
+const styles: Styles<Theme, any> = {
+  gridContainer: { width: "100% !important" },
+  form_control: { width: "100%" }
+};
+
+const useStyles = makeStyles(styles);
 
 export const JoinClassesListAsUser = (props: any) => {
   const isSmall = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
@@ -45,34 +53,86 @@ export const JoinClassesListAsAdmin = (props: any) => {
   );
 }
 
-export const JoinClassesCreate = (props: any) => (
+export const JoinClassesCreate = (props: any) => {
+  const classes = useStyles();
+
+  return (
   <Create {...props}>
     <SimpleForm>
-      <TextInput source="class_name" label="Class" validate={required()} />
-      <TextInput source="zoomid" label="Zoom ID" />
-      <TextInput source="zoom_pass" label="Zoom Password" />
-      <TextInput source="zoom_link" label="Zoom Link" />
-      <TextInput source="gclass_link" label="Google Classroom Link" />
-      <TextInput source="gclass_code" label="Google Classroom Code" />
-      <TextInput source="gdoc_link" label="Google Doc Link" />
-      <TextInput source="groupchat_link" label="Group Chat Link" />
-      <TextInput source="gdrive_link" label="Google Drive Link" />
+      <Typography variant="h6" gutterBottom>
+        Class Information
+      </Typography>
+      <Grid container spacing={3} className={classes.gridContainer}>
+        <Grid item sm={12}>
+          <TextInput source="class_name" label="Class" validate={required()} className={classes.form_control} />
+        </Grid>
+        <Grid item sm={6}>
+          <TextInput source="zoomid" label="Zoom ID" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={6}>
+          <TextInput source="zoom_pass" label="Zoom Password" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="zoom_link" label="Zoom Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={9}>
+          <TextInput source="gclass_link" label="Google Classroom Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={3}>
+          <TextInput source="gclass_code" label="Google Classroom Code" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="gdoc_link" label="Google Doc Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="groupchat_link" label="Group Chat Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="gdrive_link" label="Google Drive Link" className={classes.form_control} />
+        </Grid>
+      </Grid>
     </SimpleForm>
   </Create>
-);
+)};
 
-export const JoinClassesEdit = (props: any) => (
+export const JoinClassesEdit = (props: any) => {
+  const classes = useStyles();
+  
+  return (
   <Edit {...props}>
     <SimpleForm>
-      <TextInput source="class_name" label="Class" validate={required()} />
-      <TextInput source="zoomid" label="Zoom ID" />
-      <TextInput source="zoom_pass" label="Zoom Password" />
-      <TextInput source="zoom_link" label="Zoom Link" />
-      <TextInput source="gclass_link" label="Google Classroom Link" />
-      <TextInput source="gclass_code" label="Google Classroom Code" />
-      <TextInput source="gdoc_link" label="Google Doc Link" />
-      <TextInput source="groupchat_link" label="Group Chat Link" />
-      <TextInput source="gdrive_link" label="Google Drive Link" />
+    <Typography variant="h6" gutterBottom>
+        Class Information
+      </Typography>
+      <Grid container spacing={3} className={classes.gridContainer}>
+        <Grid item sm={12}>
+          <TextInput source="class_name" label="Class" validate={required()} className={classes.form_control} />
+        </Grid>
+        <Grid item sm={6}>
+          <TextInput source="zoomid" label="Zoom ID" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={6}>
+          <TextInput source="zoom_pass" label="Zoom Password" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="zoom_link" label="Zoom Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={9}>
+          <TextInput source="gclass_link" label="Google Classroom Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={3}>
+          <TextInput source="gclass_code" label="Google Classroom Code" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="gdoc_link" label="Google Doc Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="groupchat_link" label="Group Chat Link" className={classes.form_control} />
+        </Grid>
+        <Grid item sm={12}>
+          <TextInput source="gdrive_link" label="Google Drive Link" className={classes.form_control} />
+        </Grid>
+      </Grid>
     </SimpleForm>
   </Edit>
-);
+)};
